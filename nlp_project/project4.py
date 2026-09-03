e1e7d42becf65598f178c85a72f6d1a343cdf2c2
Romaan55 <mromaan005@gmail.com>
2026-09-03 16:33:47 +0500

    Changes in code

--- nlp_project/project4.py
+++ nlp_project/project4.py
@@ -17,11 +17,11 @@ start_index = 0
 for chunk in chunks:
     start_index = article.find(chunk, start_index)
     end_index = start_index + len(chunk)
-
+    
+    start_index = end_index - overlap
+    chunk_id += 1
+    
     print("Chunk ID:", chunk_id)
     print("Start Index:", start_index)
     print("End Index:", end_index)
     print("Text:", chunk)
-
-    start_index = end_index - overlap
-    chunk_id += 1
