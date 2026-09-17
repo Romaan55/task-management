c235ee553c9507a652fcd0f0671ffa784b09636d
Romaan55 <mromaan005@gmail.com>
2026-09-17 18:59:59 +0500

    Program 6

--- nlp_project/project6.py
+++ nlp_project/project6.py
@@ -7,15 +7,17 @@ documents = [
 
 inverted_index = {}
 # Inverted Index
+inverted_index = {}
+
 for document in documents:
     doc_id = document["doc_id"]
     text = document["text"]
     words = text.lower().split()
-for word in words:
-    if word not in inverted_index:
-        inverted_index[word] = []
-    if doc_id not in inverted_index[word]:
-        inverted_index[word].append(doc_id)
+    for word in words:
+        if word not in inverted_index:
+            inverted_index[word] = []
+        if doc_id not in inverted_index[word]:
+            inverted_index[word].append(doc_id)
 
 # Query Matching
 query = input("Enter the query: ") 
@@ -36,19 +38,31 @@ for word in query_words:
         print("Word is not available in documents")
 
 print("Matching Documents:", results)
+
 # TF Calculation
 for doc_id in results:
     for document in documents:
         if document["doc_id"] == doc_id:
-            text = document["text"].lower()
+            text = document["text"]
+            word = text.lower()
             words = text.split()
             total_words = len(words)
-    for word in query_words:
-        frequency = words.count(word)
-        tf = frequency / total_words
-           
+            for word in query_words:
+                frequency = words.count(word)
+                tf = frequency / total_words
+            
+# IDF Calculation
+total_documents = len(documents)
+
+for word in query_words:
+    if word in inverted_index:
+        document_frequency = len(inverted_index[word])
+        idf = total_documents / document_frequency
+
         print("Word:", word)
         print("Document:", doc_id)
+        print("Document Frequency:", document_frequency)
         print("TF:", tf)
+        print("IDF:", idf)
         print("Frequency",frequency)
         
