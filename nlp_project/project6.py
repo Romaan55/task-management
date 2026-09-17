47e861d2bbcfab78ae87d9cb5d21a2465ccf66fb
Romaan55 <mromaan005@gmail.com>
2026-09-17 17:31:56 +0500

    program 6

---
+++ nlp_project/project6.py
@@ -0,0 +1,55 @@
+documents = [
+    {"doc_id": "doc_1", "text": "AI is used in worldwide"},
+    {"doc_id": "doc_2", "text": "Python is also used in AI"},
+    {"doc_id": "doc_3", "text": "Linux is used in System Administration"},
+    {"doc_id": "doc_4", "text": "Python is Programming Language"}
+]
+
+inverted_index = {}
+# Inverted Index
+for document in documents:
+    doc_id = document["doc_id"]
+    text = document["text"]
+    words = text.lower().split()
+for word in words:
+    if word not in inverted_index:
+        inverted_index[word] = []
+    if doc_id not in inverted_index[word]:
+        inverted_index[word].append(doc_id)
+
+# Query Matching
+query = input("Enter the query: ") 
+query_words = query.lower().split()
+
+results = []
+
+for word in query_words:
+    if word in inverted_index:
+        print("Word:", word)
+        print("Documents:", inverted_index[word])
+
+        for doc_id in inverted_index[word]:
+            if doc_id not in results:
+                results.append(doc_id)
+    else:
+        print("Word:", word)
+        print("Word is not available in documents")
+
+print("Matching Documents:", results)
+# TF Calculation
+for doc_id in results:
+    for document in documents:
+        if document["doc_id"] == doc_id:
+            text = document["text"].lower()
+            words = text.split()
+            total_words = len(words)
+    for word in query_words:
+        frequency = words.count(word)
+        tf = frequency / total_words
+    
+        print
+        (
+            "Word:", word,
+            "Document:", doc_id,
+            "TF:", tf
+        )
